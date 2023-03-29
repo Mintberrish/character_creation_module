c9199cd7c5fb4cfe0fc7c83a9051a63fc41335ea
Пересекин Виктор Сергеевич <peresekin_vs@corp.tander.ru>
2023-03-29 22:09:05 +0300

    code according to PEP8

--- test_module.py
+++ test_module.py
@@ -8,6 +8,5 @@ files_list = ['main.py', 'readme.md']
 
 
 def test_program():
-    for filename in files_list:
-        assert filename in dir_files, f'Файл `{filename}` не найден в корне репозитория'
-
+    for file in files_list:
+        assert file in dir_files, f'Файл `{file}` не найден в корне папки'
